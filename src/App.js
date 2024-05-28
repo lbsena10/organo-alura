0c96e92866765254bda73b0e2c2d49a0fc80ec7f
lbsena10 <lucas.lbsena@gmail.com>
2024-05-27 23:51:37 -0300

    Finalizando o form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { useState } from 'react'
 import Banner from './compontentes/Banner'
 import CampoTexto from './compontentes/CampoTexto'
 import Formulario from './compontentes/Formulario'
 
 function App() {
+	const [colaboradores, setColaboradores] = useState([])
+
+	const aoNovoColaboradorAdicionado = (colaborador) => {
+		setColaboradores([...colaboradores, colaborador])
+	}
 	return (
 		<div className="App">
 			<Banner />
-			<Formulario />
+			<Formulario
+				aoColaboradorCadastrado={(colaborador) =>
+					aoNovoColaboradorAdicionado(colaborador)
+				}
+			/>
 		</div>
 	)
 }
